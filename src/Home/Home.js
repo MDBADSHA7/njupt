6792abb45b3559fd7ffe1979565367a57eb5cf2c
MDBADSHA7 <mahabubhossainfozlu@gmail.com>
2022-12-24 02:48:22 +0800

    first commit

---
+++ src/Home/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h1>MD BADSHA</h1>
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/firebase.init.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyAuynUW5b47XpIOCiFWVTPl5g99kyLYF7Q",
+    authDomain: "njupt-7c8c8.firebaseapp.com",
+    projectId: "njupt-7c8c8",
+    storageBucket: "njupt-7c8c8.appspot.com",
+    messagingSenderId: "895309063005",
+    appId: "1:895309063005:web:1ee407d3862470fb40beb4"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
